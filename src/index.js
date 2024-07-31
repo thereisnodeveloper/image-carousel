17ae88e82987542419d11b9a9fd2f03edc921303
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-07-31 18:47:25 -0400

    set up file structure, image loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-function add(a, b) {
-  return a + b;
-}
 
-console.log(add(2, 3));
+// div with display:flex
+
+// initiate by setting carousel to first image
+
+// generate array that contains amount of movement required
+
+// function to move carousel
+// read the current image's size
