34886531b4b48f96c8c85f1be5cb9381ca647049
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-08-01 10:59:40 -0400

    feat: Add image carousel functionality and styling functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+//TODO: import image
+import carouselImage1 from './images/100x200.svg';
+import carouselImage2 from './images/600x400.svg';
+
+const testElem = document.createElement('div');
+
+function addImagesToCarousel() {
+  // #region configs
+  const imageBank = [carouselImage1, carouselImage2];
+  const numImagesToAdd = 5;
+  // #endregion
+
+  for (let i = 0; i < numImagesToAdd; i += 1) {
+    const imgToAdd = new Image();
+    const rand01 = Math.floor(Math.random());
+    imgToAdd.src = imageBank[rand01];
+    
+    styleImages(imgToAdd);
+    
+    document.body.appendChild(imgToAdd);
+  }
+
+  function styleImages(img) {
+    img.style.border = 'solid 1px black';
+  }
+}
+addImagesToCarousel();
 
 // div with display:flex
 
