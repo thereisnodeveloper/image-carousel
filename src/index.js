b28345bc73f544be62f22bf2a14d8be8eab5233b
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-08-01 11:48:05 -0400

    save before trying moveByX method

--- src/index.js
+++ src/index.js
@@ -1,42 +1,58 @@
-//TODO: import image
+// [x]TODO: import image
+// [ ]TODO: create viewing window
+// [ ]TODO: measure length of each image
+// [ ]TODO: initiate carousel by going to first image
+
 import carouselImage1 from './images/100x200.svg';
 import carouselImage2 from './images/600x400.svg';
 
 const testElem = document.createElement('div');
 
-function addImagesToCarousel() {
-  // #region configs
-  const imageBank = [carouselImage1, carouselImage2];
-  const numImagesToAdd = 5;
-  // #endregion
+class Carousel {
+  // constructor(){
 
-  function styleImages(img) {
-    const newImg = img.cloneNode(true);
-    newImg.style.border = 'solid 1px black';
-    return newImg;
+  // }
+  static imagesArray = [];
+
+  static addImagesToCarousel() {
+    // #region configs
+
+    const imageBank = [carouselImage1, carouselImage2];
+    const numImagesToAdd = 5;
+    // #endregion
+    function styleImages(img) {
+      const newImg = img.cloneNode(true);
+      newImg.style.border = 'solid 1px black';
+      return newImg;
+    }
+    /** * create carousel to hold images */
+    function createCarousel() {
+      const carousel = document.createElement('div');
+      carousel.classList.add('.carousel');
+      carousel.style.display = 'flex';
+      carousel.style.flex = 1;
+      return carousel;
+    }
+    const carousel = createCarousel();
+
+    for (let i = 0; i < numImagesToAdd; i += 1) {
+      let imgToAdd = new Image();
+      const rand01 = Math.round(Math.random());
+      imgToAdd.src = imageBank[rand01];
+
+      imgToAdd = styleImages(imgToAdd);
+      this.imagesArray.push(imgToAdd);
+      carousel.appendChild(imgToAdd);
+    }
+    document.body.appendChild(carousel);
   }
-  /** * create carousel to hold images */
-  function createCarousel() {
-    const carousel = document.createElement('div');
-    carousel.classList.add('.carousel');
-    carousel.style.display = 'flex';
-    carousel.style.flex = 1;
-    return carousel;
-  }
-  const carousel = createCarousel();
 
-  for (let i = 0; i < numImagesToAdd; i += 1) {
-    let imgToAdd = new Image();
-    const rand01 = Math.round(Math.random());
-    imgToAdd.src = imageBank[rand01];
-
-    imgToAdd = styleImages(imgToAdd);
-
-    carousel.appendChild(imgToAdd);
-  }
-  document.body.appendChild(carousel);
+  static makeViewingWindow() {}
 }
-addImagesToCarousel();
+
+Carousel.addImagesToCarousel();
+
+// Carousel.addImagesToCarousel();
 
 // div with display:flex
 
