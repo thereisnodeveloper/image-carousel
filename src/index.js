3e27004c857c4fe3f614391dd0089adc968c0ee8
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-08-01 21:18:39 -0400

    chore: Refactor image styling and adding viewing window functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 // [x]TODO: import image
 // [ ]TODO: create viewing window
+//CURRENT: figure out how to make viewing window same size as 1 image
 // [ ]TODO: measure length of each image
 // [ ]TODO: initiate carousel by going to first image
+
+// commit: no longer look Carousel's array to calculate width/height, since
+// image size may change after appending to carousel, and array has reference to
+// image AT its creation
 
 import carouselImage1 from './images/300x200.svg';
 import carouselImage2 from './images/600x400.svg';
@@ -12,11 +17,9 @@ class Carousel {
   // constructor(){
 
   // }
-  static imagesArray = [];
 
   static addImagesToCarousel() {
     // #region configs
-
     const imageBank = [carouselImage1, carouselImage2];
     const numImagesToAdd = 5;
     // #endregion
@@ -37,27 +40,56 @@ class Carousel {
       carousel.style.border = '1px solid red';
       return carousel;
     }
-    const carousel = createCarousel();
-
+    this.carousel = createCarousel();
     for (let i = 0; i < numImagesToAdd; i += 1) {
       let imgToAdd = new Image();
       const rand01 = Math.round(Math.random());
       imgToAdd.src = imageBank[rand01];
 
-      imgToAdd = styleImages(imgToAdd);
-      this.imagesArray.push(imgToAdd);
-      carousel.appendChild(imgToAdd);
+      // imgToAdd = styleImages(imgToAdd);
+      imgToAdd.style.display = 'flex';
+      let imgWrapper = document.createElement('div');
+      imgWrapper.appendChild(imgToAdd);
+      imgWrapper = styleImages(imgWrapper);
+
+      this.carousel.appendChild(imgWrapper);
     }
-    document.body.appendChild(carousel);
+    document.body.appendChild(this.carousel);
   }
 
-  // static makeViewingWindow() {
-  // }
+  static get widthHeight() {
+    console.log(this.carousel.firstChild);
 
-  static measureImages() {}
+    const [height, width] = [
+      this.carousel.firstChild.offsetHeight,
+      this.carousel.firstChild.offsetWidth,
+    ];
+    return [width, height];
+  }
+
+  static makeViewingWindow() {
+    const viewingWindow = document.createElement('div');
+    const vwStyle = viewingWindow.style;
+    [vwStyle.width, vwStyle.height] = this.widthHeight;
+    // vwStyle.height = `${this.imagesArray[0].height}px`;
+    vwStyle.border = 'solid 5px green';
+    // viewingWindow.textContent = ' ';
+    // viewingWindow.style.zIndex = 2;
+    // this.carousel.appendChild(viewingWindow);
+    document.body.appendChild(viewingWindow);
+  }
+
+  static get imgWidth() {
+    return this.imagesArray[0].style.width;
+  }
+
+  static get imgHeight() {
+    return this.imagesArray[0].style.height;
+  }
 }
 
 Carousel.addImagesToCarousel();
+Carousel.makeViewingWindow();
 
 // Carousel.addImagesToCarousel();
 
